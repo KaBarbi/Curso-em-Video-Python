3aad028dd35c159178f3757f518c5820049ac2f1
Kaue Barbi <kabarbi@localhost.localdomain>
2024-08-18 14:57:06 -0300

    ex 36 e 37 inicio

--- Mundo2PythonExercicios/ex36.py
+++ Mundo2PythonExercicios/ex36.py
@@ -2,3 +2,8 @@
 #O programa vai perguntar o valor da casa, o salario do comprador e em quantos anos ele vai pagar.
 #Calcule o valor da prestação mensal, sabando que não pdoe exeder 30% do salario ou então o emprestimo será negado.
 
+vcasa = float(input('Qual o valor da casa? '))
+sal = float(input('Qual seu salario? '))
+anos = int(input('Em quantos anos vai pagar? '))
+
+pmensal =
--- Mundo2PythonExercicios/ex37.py
+++ Mundo2PythonExercicios/ex37.py
@@ -2,3 +2,10 @@
 #1 para binário
 #2 para octal
 #3 para hexadecimal
+
+n = float(input('Digite um numero: '))
+escolha = int(input('Escolha qual base de conversão iŕa usar: 1- Binario, 2- Octal, 3- hexadecimal: '))
+
+if escolha == 1:
+    binario = bin(n)
+    print(binario)
